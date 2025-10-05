6b6f3a87dc887efd033c6efb3bb6c33f7430845a
JVinrey <139847321+JVinrey@users.noreply.github.com>
2025-10-05 01:17:28 -0500

    Agrego carpeta Websocket y archivos

---
+++ Websocket/src/websockets/websocket.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { WebsocketGateway } from "./websocket.gateway";
+
+@Module({
+    providers: [WebsocketGateway]
+})
+
+
+
+export class GatewayModule{
+
+}
